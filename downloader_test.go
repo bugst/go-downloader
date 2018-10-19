63d2d032858dea8ca84a1b038765d2bb9c208ab0
Cristian Maglie <c.maglie@bug.st>
2018-10-19 11:41:52 +0200

    Added test for RunAndPool

--- downloader_test.go
+++ downloader_test.go
@@ -7,9 +7,11 @@
 package downloader
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -68,4 +70,23 @@ func TestInvalidRequest(t *testing.T) {
 	d, err := Download(tmpFile, "asd://go.bug.st/test.txt")
 	require.Error(t, err)
 	require.Nil(t, d)
+}
+
+func TestRunAndPool(t *testing.T) {
+	tmpFile := makeTmpFile(t)
+	defer os.Remove(tmpFile)
+
+	d, err := Download(tmpFile, "https://downloads.arduino.cc/cores/avr-1.6.20.tar.bz2")
+	require.NoError(t, err)
+	prevCurr := int64(0)
+	callCount := 0
+	callback := func(curr int64) {
+		require.True(t, prevCurr <= curr)
+		prevCurr = curr
+		callCount++
+	}
+	d.RunAndPoll(callback, time.Millisecond)
+	fmt.Printf("callback called %d times\n", callCount)
+	require.True(t, callCount > 10)
+	require.Equal(t, int64(4897949), d.Completed())
 }
