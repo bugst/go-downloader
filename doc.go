e2b3286cbacc6ede2f8bbeae2ed31d528a288ea8
Cristian Maglie <c.maglie@bug.st>
2026-01-09 17:59:05 +0100

    Updated license text

---
+++ doc.go
@@ -0,0 +1,9 @@
+//
+// Copyright 2018-2025 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+// Package downloader provides an asynchronous file downloader with
+// progress tracking and timeout support.
+package downloader
